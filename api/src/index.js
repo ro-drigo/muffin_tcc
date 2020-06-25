a5669e48b3e2cb624c62252d1ec2bfb4fafb8b60
ro-drigo <rodrigo.santos7202@gmail.com>
2020-06-25 19:42:28 -0300

    connection with pg db = ok.

--- api/src/index.js
+++ api/src/index.js
@@ -1,11 +1,14 @@
 const express = require('express')
+//importando o knex para usar
+const knex = require('./database')
 
 const app = express()
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send("Beggining")
+//testando a conexão
+app.get('/users', (req, res) => {
+    knex('pessoa').then((results) => res.json(results))
 })
 
 app.listen(3000)
