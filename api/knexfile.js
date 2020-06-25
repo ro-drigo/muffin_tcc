3720ec4dc9129462489afa8c114b9b85d6fef9b0
ro-drigo <rodrigo.santos7202@gmail.com>
2020-06-25 19:24:16 -0300

    create and config knexfile

---
+++ api/knexfile.js
@@ -0,0 +1,15 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      database: "Muffin",
+      user: "postgres",
+      password: "123456"
+    }
+  }
+}
+
+ 
