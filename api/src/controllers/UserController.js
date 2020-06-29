ac0ae70444cf737a1433c3a7ebc565d3ca14d4b9
ro-drigo <rodrigo.santos7202@gmail.com>
2020-06-29 18:50:34 -0300

    catch all middleware

--- api/src/controllers/UserController.js
+++ api/src/controllers/UserController.js
@@ -3,80 +3,94 @@ const knex = require('../database')
 
 module.exports = {
     //listar todos usuários
-    async index(req, res) {
-        const results = await knex('pessoa')
+    async index(req, res, next) {
+        try{
+            const results = await knex('pessoa')
 
-        return res.json(results)
+            return res.json(results)
+        }catch (error){
+            next(error)
+        }
     },
 
     //listar apenas um usuário
-    async show(req, res) {
-        const { id } = req.params
+    async show(req, res, next) {
 
-        const result = await knex('pessoa').where('id_pes', id)
+        try{
+            const { id } = req.params
 
-        return res.json(result)
+            const result = await knex('pessoa').where('id_pes', id)
+
+            return res.json(result)
+        }catch (error) {
+            next(error)
+        }
             
         
     },
 
     //cadastrar um usuário
     async create(req, res) {
-        const { 
-            nome,
-            cpf,
-            cidade,
-            rua,
-            complemento,
-            celular,
-            email,
-            datanasc,
-            cep,
-            estado,
-            numero,
-            bairro,
-            telefone,
-            sal,
-            senha
+        try{
+            const { 
+                nome,
+                cpf,
+                cidade,
+                rua,
+                complemento,
+                celular,
+                email,
+                datanasc,
+                cep,
+                estado,
+                numero,
+                bairro,
+                telefone,
+                sal,
+                senha
 
-         } = req.body
+            } = req.body
 
-         //registrando a cidade
-        const regist_cidade = await knex('cidade').insert(
-            { 
-              nome_city: cidade,
-              id_uf: estado
-            }
-        )
+            //registrando a cidade
+            const regist_cidade = await knex('cidade').insert(
+                { 
+                    nome_city: cidade,
+                    id_uf: estado
+                }
+            )
 
-         //registrando o endereço
-        const regist_endereco = await knex('endereco').insert(
-            { 
-              id_cep: cep,
-              lograd_end: rua, 
-              bairro_end: bairro, 
-              id_city: knex('cidade').max('id_city')
-            }
-        )
+            //registrando o endereço
+            const regist_endereco = await knex('endereco').insert(
+                { 
+                    id_cep: cep,
+                    lograd_end: rua, 
+                    bairro_end: bairro, 
+                    id_city: knex('cidade').max('id_city')
+                }
+            )
 
-         //registrando na tabela pessoa
-        const regist_pessoa = await knex('pessoa').insert(
-             { 
-                 cpf_pes: cpf, 
-                 nome_pes: nome, 
-                 date_nasc: datanasc, 
-                 sal_pes: sal, 
-                 id_cep: cep,
-                 comp_pes: complemento,
-                 num_pes: numero,
-                 cel_pes: celular,
-                 tel_com: telefone,
-                 email_pes: email,
-                 pass_pes: senha
-             }
-        )
+            //registrando na tabela pessoa
+            const regist_pessoa = await knex('pessoa').insert(
+                { 
+                    cpf_pes: cpf, 
+                    nome_pes: nome, 
+                    date_nasc: datanasc, 
+                    sal_pes: sal, 
+                    id_cep: cep,
+                    comp_pes: complemento,
+                    num_pes: numero,
+                    cel_pes: celular,
+                    tel_com: telefone,
+                    email_pes: email,
+                    pass_pes: senha
+                }
+            )
 
-         return res.json({ ok: "usuário cadastrado" })
+            return res.json({ ok: "usuário cadastrado" })
+
+        }catch (error) {
+            next(error)
+        }
     }
  }
 
--- api/src/index.js
+++ api/src/index.js
@@ -7,5 +7,11 @@ app.use(express.json())
 
 app.use(routes)
 
+//catch all para pegar todos erros (middleware)
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+    res.json({ error: error.message })
+})
+
 
 app.listen(3000)
